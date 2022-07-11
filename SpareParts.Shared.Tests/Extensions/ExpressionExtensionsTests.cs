be1c2be099007671360dcc173ee3786e4c91d4d6
sjmarsh <sjmarsh31@gmail.com>
2022-07-12 09:12:16 +1000

    Refactor ValidInputNumberFormat - Remove Modal Param - we can get this from EditContext - Move Expression helpers into separate extensions class - Modal Close button text parameter

---
+++ SpareParts.Shared.Tests/Extensions/ExpressionExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using SpareParts.Shared.Extensions;
+using System.Linq.Expressions;
+
+namespace SpareParts.Shared.Tests.Extensions
+{
+    public class ExpressionExtensionsTests
+    {
+        [Fact]
+        public void ShouldReturnCompiledValue()
+        {
+            const string testName = "Test";
+            var tm = new TestModel { Id = 1, Name = testName };
+            Expression<Func<string>> exp = () => tm.Name;
+
+            var result = exp.GetValueFromExpression();
+            
+            Assert.Equal(testName, result);
+        }
+
+        [Fact]
+        public void ShouldReturnCompiledValueWhenNull()
+        {            
+            var tm = new TestModel();
+            Expression<Func<string>> exp = () => tm.Name;
+
+            var result = exp.GetValueFromExpression();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ShoudlReturnMemberName()
+        {
+            var tm = new TestModel();
+            Expression<Func<DateTime?>> exp = () => tm.DateOfBirth;
+
+            var result = exp.GetMemberNameFromExpression();
+
+            Assert.Equal("DateOfBirth", result);
+        }
+
+        [Fact]
+        public void ShouldSetValueToExpression()
+        {
+            const string value = "Test";
+            var tm = new TestModel();
+            Expression<Func<string>> exp = () => tm.Name;
+
+            exp.SetValueToExpression(tm, value);
+
+            Assert.Equal(value, tm.Name);
+        }
+
+        public class TestModel
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+        }
+    }
+}
---
+++ SpareParts.Shared/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpareParts.Shared.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        public static T GetValueFromExpression<T>(this Expression<Func<T>> expression)
+        {
+            var compiled = expression.Compile();
+            return compiled();
+        }
+
+        public static string GetMemberNameFromExpression<TValue>(this Expression<Func<TValue>> expression)
+        {
+            return GetMemberInfoFromExpression(expression).Name;
+        }
+
+        public static void SetValueToExpression<TValue>(this Expression<Func<TValue>> expression, object model, TValue value)
+        {
+            var prop = GetMemberInfoFromExpression(expression) as PropertyInfo;
+            prop?.SetValue(model, value, null);
+        }
+
+        private static MemberInfo GetMemberInfoFromExpression<TValue>(Expression<Func<TValue>> expression)
+        {
+            MemberExpression forMember = expression.Body switch
+            {
+                MemberExpression memberExpression => memberExpression,
+                UnaryExpression { Operand: MemberExpression unaryMemberExpression } => unaryMemberExpression,
+                _ => throw new InvalidOperationException("Must be an expression terminating in a member")
+            };
+
+            return forMember.Member;
+        }
+    }
+}
