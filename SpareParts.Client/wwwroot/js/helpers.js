2308fe5d2baef9bef20f88d1098fb3f3322f9c81
sjmarsh <sjmarsh31@gmail.com>
2022-07-17 16:07:59 +1000

    Initial work on Inventory UI

---
+++ SpareParts.Client/wwwroot/js/helpers.js
@@ -0,0 +1,18 @@
+﻿export function setFocus(elementId) {
+    var el = document.getElementById(elementId);
+    if (el) {
+        el.focus();
+    }
+}
+
+export function scrollIntoView(elementId) {
+    var el = document.getElementById(elementId);
+    if (el) {
+        el.scrollIntoView();
+    }
+}
+
+export function selectTab(elementId) {
+    setFocus(elementId);
+    scrollIntoView(elementId);
+}
