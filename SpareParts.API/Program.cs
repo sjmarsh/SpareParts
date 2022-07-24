607a4bcbc349a9f879ac055c90e61957c5743fff
sjmarsh <sjmarsh31@gmail.com>
2022-07-24 16:19:13 +1000

    Allow CORS for dev

--- SpareParts.API/Program.cs
+++ SpareParts.API/Program.cs
@@ -23,7 +23,7 @@ try
     builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
     // Add services to the container.
-
+    
     builder.Services.AddControllers()
         .AddJsonOptions(options =>
             options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()
@@ -56,10 +56,16 @@ try
 
 
     var app = builder.Build();
-
+        
+    
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
+        app.UseCors(builder => builder
+         .AllowAnyOrigin()
+         .AllowAnyMethod()
+         .AllowAnyHeader());
+
         app.UseSwagger();
         app.UseSwaggerUI();
 
@@ -74,11 +80,11 @@ try
     }
 
     app.UseHttpsRedirection();
-
+        
     // For Blazor
     app.UseBlazorFrameworkFiles();
     app.UseStaticFiles();
-   
+
     app.UseAuthorization();
 
     //app.MapControllers();
