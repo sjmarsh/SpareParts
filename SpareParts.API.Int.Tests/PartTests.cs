2111b4fb5f038e609b5af63f3472e899a73d27f3
sjmarsh <sjmarsh31@gmail.com>
2022-08-05 13:32:26 +1000

    Complete Integration Tests for Parts API

--- SpareParts.API.Int.Tests/PartTests.cs
+++ SpareParts.API.Int.Tests/PartTests.cs
@@ -1,120 +1,167 @@
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using SpareParts.API.Data;
 using SpareParts.Shared.Models;
-using System.Net.Http.Headers;
-using System.Text;
 
 namespace SpareParts.API.Int.Tests
 {
-    public class PartTests : IDisposable
+    [Collection("Spare Parts Tests")]
+    public class PartTests 
     {
-        //ref:
-        //https://www.thinktecture.com/en/entity-framework-core/isolation-of-integration-tests-in-2-1/
-        //https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
+        private readonly SparePartsTestFixture _testFixture;
 
-
-        const string _databaseName = "SpareParts_Test";
-        SparePartsDbContext _dbContext;
-        HttpClient _client;
-
-        public PartTests()
+        public PartTests(SparePartsTestFixture testFixture)
         {
-            var options = new DbContextOptionsBuilder<SparePartsDbContext>()
-                                .UseSqlServer($"Server=SJM-ULTRABOOK;Database={_databaseName};Trusted_Connection=True")
-                                .Options;
-            _dbContext = new SparePartsDbContext(options);
-            _dbContext.Database.Migrate();
+            _testFixture = testFixture;
 
-            var application = new CustomWebApplicationFactory<Program>()
-                                    .WithWebHostBuilder(builder =>
-                                    {
-                                        // ... Configure test services
-                                        builder.ConfigureServices(services =>
-                                        {   
-                                            
-                                            //services.AddDbContext<SparePartsDbContext>(options => options.UseSqlServer($"Server=SJM-ULTRABOOK;Database={_databaseName};Trusted_Connection=True"));
-                                        });
-                                        
-                                    });
+            // clear parts table between tests
+            _testFixture.DbContext.Parts.RemoveRange(_testFixture.DbContext.Parts);
+            _testFixture.DbContext.SaveChanges();
+        }
 
-            _client = application.CreateClient();
+        [Fact]
+        public async void Get_Should_ReturnRequiredPartRecord()
+        {
+            var savedPart = await CreatePartInDatabase();
+            savedPart?.ID.Should().BeGreaterThan(0);
 
+            var result = await _testFixture.GetRequest<PartResponse>($"/api/part/?id={savedPart.ID}");
             
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+            result.Value.Should().BeEquivalentTo(savedPart);
         }
 
-        public void Dispose()
+        [Fact]
+        public async void GetIndex_Should_ReturnListOfParts()
         {
-            _dbContext.Database.EnsureDeleted();
-            _dbContext.Database.CloseConnection();
-            _client.Dispose();
+            var parts = await CreateListInDatabase(5);
+                        
+            var result = await _testFixture.GetRequest<PartListResponse>($"/api/part/index");
+
+            result.Should().NotBeNull();
+            result.Items.Should().NotBeNull();
+            result.Items.Should().HaveCount(5);
+            result.Items.Should().BeEquivalentTo(parts);
         }
+
+        [Fact]
+        public async void GetIndex_Should_ReturnListOfPartsWithRequiredPaging()
+        {
+            await CreateListInDatabase(5);
+
+            var result = await _testFixture.GetRequest<PartListResponse>($"/api/part/index?skip=2&take=2");
+
+            result.Should().NotBeNull();
+            result.Items.Should().NotBeNull();
+            result.Items.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public async void GetIndex_Should_ReturnListOfCurrentParts()
+        {
+            var parts = GetPartsFakerConfig().Generate(3);
+            parts[0].StartDate = DateTime.Today.AddYears(-1); // current
+            parts[0].EndDate = null;
+            parts[1].StartDate = DateTime.Today.AddYears(-1); // not current
+            parts[1].EndDate = DateTime.Today.AddDays(-2);
+            parts[2].StartDate = DateTime.Today.AddYears(1); // not current
+            parts[2].EndDate = DateTime.Today.AddDays(2);
+
+            foreach (var part in parts)
+            {
+                _testFixture.DbContext.Parts.Add(part);
+            }
+            await _testFixture.DbContext.SaveChangesAsync();
+
+            var result = await _testFixture.GetRequest<PartListResponse>($"/api/part/index?isCurrentOnly=true");
+
+            result.Should().NotBeNull();
+            result.Items.Should().NotBeNull();
+            result.Items.Should().HaveCount(1);
+            result.Items[0].Should().BeEquivalentTo(parts[0]);
+        }
+
 
         [Fact]
         public async void Post_Should_CreatePartRecord()
         {
             var part = new Part { Name = "Part 1", Description = "One", Weight = 1.1, Price = 2.2, StartDate = DateTime.Today.AddYears(-1) };
-            var result = await PostRequest<Part, PartResponse>("/api/part", part);
-            Assert.NotNull(result);
-            Assert.NotNull(result.Value);
-            var partId = result.Value.ID;
-            Assert.True(partId > 0);
-            var savedPart = _dbContext.Parts.Find(partId);
-            Assert.NotNull(savedPart);
-            Assert.Equal(part.Name, savedPart.Name);
-            Assert.Equal(part.Description, savedPart.Description);
-
-
-        }
-
-        private async Task<TOut> PostRequest<TIn, TOut>(string uri, TIn content) where TOut : new()
-        {
-            try
-            {
-                //using (var client = new HttpClient())
-                //{
-                    _client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/json"));
-
-                    var serialized = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
-
-                    using (HttpResponseMessage response = await _client.PostAsync(uri, serialized))
-                    {
-                        response.EnsureSuccessStatusCode();
-                        string responseBody = await response.Content.ReadAsStringAsync();
-
-                        return JsonConvert.DeserializeObject<TOut>(responseBody);
-                    }
-                //}
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.ToString());
-                Console.WriteLine(ex.ToString());
-            }
-
-            return await Task.FromResult(new TOut());
+                        
+            var result = await _testFixture.PostRequest<Part, PartResponse>("/api/part", part);
             
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+            
+            var partId = result?.Value?.ID;
+            Assert.True(partId > 0);
+            var savedPart = await _testFixture.DbContext.Parts.FindAsync(partId);
+            
+            savedPart.Should().NotBeNull();
+            savedPart.Should().BeEquivalentTo(part, opt => opt.Excluding(p => p.ID));
         }
 
-    }
-
-
-    // ref: Issue with not picking up custom app settings for tests
-    // https://github.com/dotnet/aspnetcore/issues/38435
-    public class CustomWebApplicationFactory<TProgram>
-    : WebApplicationFactory<TProgram> where TProgram : class
-    {
-        protected override IHost CreateHost(IHostBuilder builder)
+        [Fact]
+        public async void Put_Should_UpdatePartRecord()
         {
-            builder.UseEnvironment("IntegrationTest");
+            var savedPart = await CreatePartInDatabase();
+            savedPart?.ID.Should().BeGreaterThan(0);
+            var partModel = new Part { ID = savedPart.ID, Name = "Other Name", Description = "Other Descritpion", Weight = savedPart.Weight, Price = savedPart.Price.Value, StartDate = savedPart.StartDate };
+            _testFixture.DbContext.ChangeTracker.Clear();
 
-            return base.CreateHost(builder);
+            var result = await _testFixture.PutRequest<Part, PartResponse>("/api/part", partModel);
+
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+
+            var partId = result?.Value?.ID;
+            Assert.True(partId > 0);
+            var updatedPart = await _testFixture.DbContext.Parts.FindAsync(partId);
+
+            updatedPart.Should().NotBeNull();
+            updatedPart.Should().BeEquivalentTo(partModel, opt => opt.Excluding(p => p.ID));
+        }
+
+        [Fact]
+        public async void Delete_Should_DeletePartRecord()
+        {
+            var savedPart = await CreatePartInDatabase();
+            savedPart?.ID.Should().BeGreaterThan(0);
+            _testFixture.DbContext.ChangeTracker.Clear();
+
+            var result = await _testFixture.DeleteRequest<PartResponse>($"/api/part/?id={savedPart.ID}");
+
+            result.Value.Should().BeNull();
+            result.HasError.Should().BeFalse();
+            var deletedPart = await _testFixture.DbContext.Parts.FindAsync(savedPart.ID);
+            deletedPart.Should().BeNull();
+        }
+
+        private async Task<Entities.Part> CreatePartInDatabase()
+        {
+            var partEntity = GetPartsFakerConfig().Generate(1).First();
+            _testFixture.DbContext.Parts.Add(partEntity);
+            await _testFixture.DbContext.SaveChangesAsync();
+            var savedPart = await _testFixture.DbContext.Parts.FindAsync(partEntity.ID);
+            return savedPart;
+        }
+
+        private async Task<List<Entities.Part>> CreateListInDatabase(int howMany)
+        {
+            var parts = GetPartsFakerConfig().Generate(howMany);
+            foreach (var part in parts)
+            {
+                _testFixture.DbContext.Parts.Add(part);
+            }
+            await _testFixture.DbContext.SaveChangesAsync();
+
+            return parts;
+        }
+
+        private static Faker<Entities.Part> GetPartsFakerConfig()
+        {
+            return new Faker<Entities.Part>()
+                        .RuleFor(p => p.ID, 0)
+                        .RuleFor(p => p.Weight, f => f.Random.Number(0, 20))
+                        .RuleFor(p => p.Price, f => f.Random.Number(0, 20))
+                        .RuleFor(p => p.StartDate, f => f.Date.Between(new DateTime(2000, 1, 1), DateTime.Today));
         }
     }
 }
---
+++ SpareParts.API.Int.Tests/SparePartsTestFixture.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using SpareParts.API.Data;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace SpareParts.API.Int.Tests
+{
+    //ref:
+    //https://www.thinktecture.com/en/entity-framework-core/isolation-of-integration-tests-in-2-1/
+    //https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
+
+    [CollectionDefinition("Spare Parts Tests")]
+    public class SparePartsTestCollection : ICollectionFixture<SparePartsTestFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+
+    public class SparePartsTestFixture : IDisposable
+    {
+        public SparePartsTestFixture()
+        {
+            DbContext = SetupDbContext();
+            HttpClient = SetupTestClient();             
+        }
+
+        public SparePartsDbContext DbContext { get; private set; }
+        public HttpClient HttpClient { get; private set; }
+
+        public async Task<TOut> GetRequest<TOut>(string uri ) where TOut : new()
+        {
+            try
+            {
+                using HttpResponseMessage response = await HttpClient.GetAsync(uri);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<TOut>(responseBody);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+
+            return await Task.FromResult(new TOut());
+        }
+
+        public async Task<TOut> PostRequest<TIn, TOut>(string uri, TIn content) where TOut : new()
+        {
+            try
+            {
+                HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var serialized = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+
+                using HttpResponseMessage response = await HttpClient.PostAsync(uri, serialized);
+                
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<TOut>(responseBody);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+
+            return await Task.FromResult(new TOut());
+        }
+
+        public async Task<TOut> PutRequest<TIn, TOut>(string uri, TIn content) where TOut : new()
+        {
+            try
+            {
+                HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var serialized = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+
+                using HttpResponseMessage response = await HttpClient.PutAsync(uri, serialized);
+
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<TOut>(responseBody);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+
+            return await Task.FromResult(new TOut());
+        }
+
+        public async Task<TOut> DeleteRequest<TOut>(string uri) where TOut : new()
+        {
+            try
+            {
+                using HttpResponseMessage response = await HttpClient.DeleteAsync(uri);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<TOut>(responseBody);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+
+            return await Task.FromResult(new TOut());
+        }
+
+        private SparePartsDbContext SetupDbContext()
+        {
+            // Use appsettings from the API project
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.IntegrationTest.json")
+                .Build();
+                        
+            var options = new DbContextOptionsBuilder<SparePartsDbContext>()
+                                .UseSqlServer(config["ConnectionStrings:SparePartsDbConnection"])
+                                .Options;
+            var dbContext = new SparePartsDbContext(options);
+            dbContext.Database.Migrate();
+            dbContext.Database.EnsureCreated();
+            return dbContext;
+        }
+
+        private HttpClient SetupTestClient()
+        {
+            var application = new CustomWebApplicationFactory<Program>()
+                                    .WithWebHostBuilder(builder =>
+                                    {
+                                        // ... Configure test services
+                                        builder.ConfigureServices(services =>
+                                        {
+                                            
+                                        });
+
+                                    });
+
+            return application.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Database.EnsureDeleted();
+            DbContext.Database.CloseConnection();
+            DbContext.Dispose();
+            HttpClient.Dispose();
+        }
+    }
+        
+    // ref: Issue with not picking up custom app settings for tests
+    // https://github.com/dotnet/aspnetcore/issues/38435
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseEnvironment("IntegrationTest");
+
+            return base.CreateHost(builder);
+        }
+    }
+
+}
