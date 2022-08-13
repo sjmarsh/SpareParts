4709ecdfa605d85d5f0c4cff12c1be28b994ced7
sjmarsh <sjmarsh31@gmail.com>
2022-08-13 10:44:50 +1000

    Add unit tests for TabControl

---
+++ SpareParts.Client.Tests/Shared/Components/TabControlTests.cs
@@ -0,0 +1,85 @@
+﻿using SpareParts.Client.Shared.Components;
+
+namespace SpareParts.Client.Tests.Shared.Components
+{
+    public class TabControlTests
+    {
+        [Fact]
+        public void Should_RenderTabsForSuppliedPages()
+        {
+            var ctx = new TestContext();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            var cut = ctx.RenderComponent<TabControl>(parameters => parameters
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 1"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 2"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 3"))
+                );
+
+            var tabs = cut.FindAll(".nav-link");
+            tabs.Should().HaveCount(3);
+            tabs[0].TextContent.Should().Be("Tab 1");
+            tabs[1].TextContent.Should().Be("Tab 2");
+            tabs[2].TextContent.Should().Be("Tab 3");
+        }
+
+        [Fact]
+        public void Should_RenderTabContent()
+        {
+            const string tabContent = "Hello. I'm inside a tab.";
+            var ctx = new TestContext();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            var cut = ctx.RenderComponent<TabControl>(parameters => parameters
+                .AddChildContent<TabPage>(childParams => childParams
+                    .Add(p => p.Text, "Tab 1")
+                    .AddChildContent($"<div class='content'>{tabContent}</div>"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 2"))
+                );
+
+            var tab = cut.Find(".content");
+
+            tab.InnerHtml.Should().Be(tabContent);
+        }
+
+        [Fact]
+        public void Should_DefaultToFirstTab()
+        {
+            var ctx = new TestContext();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            var cut = ctx.RenderComponent<TabControl>(parameters => parameters
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 1"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 2"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 3"))
+                );
+
+            var tabs = cut.FindAll(".nav-link");
+            tabs.Should().HaveCount(3);
+            tabs[0].ClassList.Should().Contain("is-selected");
+            tabs[1].ClassList.Should().NotContain("is-selected");
+            tabs[2].ClassList.Should().NotContain("is-selected");
+        }
+
+        [Fact]
+        public void Should_SelectTab()
+        {
+            var ctx = new TestContext();
+            ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+            var cut = ctx.RenderComponent<TabControl>(parameters => parameters
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 1"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 2"))
+                .AddChildContent<TabPage>(childParams => childParams.Add(p => p.Text, "Tab 3"))
+                );
+
+            var tabs = cut.FindAll(".nav-link");
+            tabs.Should().HaveCount(3);
+            tabs[0].ClassList.Should().Contain("is-selected");
+            tabs[1].ClassList.Should().NotContain("is-selected");
+
+            tabs[1].Click();
+
+            tabs = cut.FindAll(".nav-link");
+            tabs.Should().HaveCount(3);
+            tabs[0].ClassList.Should().NotContain("is-selected");
+            tabs[1].ClassList.Should().Contain("is-selected");
+        }
+    }
+}
