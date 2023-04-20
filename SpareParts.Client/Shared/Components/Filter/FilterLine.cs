f6fda019a8f7c63847672de6947bab8c3c45d64b
sjmarsh <sjmarsh31@gmail.com>
2023-04-20 16:33:04 +1000

    Add multi-line filters

---
+++ SpareParts.Client/Shared/Components/Filter/FilterLine.cs
@@ -0,0 +1,16 @@
+﻿namespace SpareParts.Client.Shared.Components.Filter
+{
+    public class FilterLine
+    {
+        public FilterLine()
+        {
+            SelectedField = "";
+            SelectedOperator = "eq";
+            Value = "";
+        }
+
+        public string SelectedField { get; set; }
+        public string SelectedOperator { get; set; }
+        public string Value { get; set; }
+    }
+}
