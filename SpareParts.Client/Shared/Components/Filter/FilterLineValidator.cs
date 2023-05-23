23540f0c1b47dbf2c05bcfbdeff5b47b6d66171e
sjmarsh <sjmarsh31@gmail.com>
2023-05-23 15:34:43 +1000

    Add FilterLine validation

---
+++ SpareParts.Client/Shared/Components/Filter/FilterLineValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace SpareParts.Client.Shared.Components.Filter
+{
+    public class FilterLineValidator : AbstractValidator<FilterLine>
+    {
+        public FilterLineValidator()
+        {
+            RuleFor(f => f.SelectedField).NotNull().NotEmpty();
+            RuleFor(f => f.SelectedOperator).NotNull().NotEmpty();
+            RuleFor(f => f.Value).NotNull().NotEmpty();
+        }
+    }
+
+    public class FilterLinesValidator : AbstractValidator<List<FilterLine>>
+    {
+        public FilterLinesValidator()
+        {
+            RuleForEach(l => l).SetValidator(new FilterLineValidator());
+        }
+    }
+}
