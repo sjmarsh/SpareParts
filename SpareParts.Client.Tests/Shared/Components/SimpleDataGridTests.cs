3f1f7b060ff1139f2e25e72200c2332c19f7bf58
sjmarsh <sjmarsh31@gmail.com>
2023-06-05 15:12:17 +1000

    Add Simple Data Grid Tests

---
+++ SpareParts.Client.Tests/Shared/Components/SimpleDataGridTests.cs
@@ -0,0 +1,109 @@
+﻿using SpareParts.Client.Shared.Components;
+
+namespace SpareParts.Client.Tests.Shared.Components
+{
+    public class SimpleDataGridTests
+    {
+        [Fact]
+        public void Should_RenderGridForGivenDataSource()
+        {
+            var theDate1 = new DateTime(2023, 06, 03);
+            var theDate2 = new DateTime(2022, 01, 06);
+
+            List<TestModel> testModelList = new()
+            {
+                new TestModel { DateVal = theDate1, NumberVal = 2, TextVal = "Hello" },
+                new TestModel { DateVal = theDate2, NumberVal = 4, TextVal = "Goodbye" }
+            };
+
+            var ctx = new TestContext();
+            var cut = ctx.RenderComponent<SimpleDataGrid<TestModel>>(parameters => parameters
+                .Add(p => p.DataSource, testModelList));
+
+            var table = cut.Find("table");
+            table.Should().NotBeNull();
+            
+            var rows = cut.FindAll("tr");
+            rows.Should().NotBeNull();
+            rows.Should().HaveCount(3);
+
+            var headers = cut.FindAll("th");
+            headers.Should().NotBeNull();
+            headers.Should().HaveCount(3);
+            headers.Select(h => h.TextContent).Should().BeEquivalentTo(new[] { "DateVal", "NumberVal", "TextVal" });
+            
+            var dataCells = cut.FindAll("td");
+            dataCells.Should().NotBeNull();
+            dataCells.Should().HaveCount(6);
+            dataCells.Select(d => d.TextContent).Should().BeEquivalentTo(new[] { theDate1.ToString(), "2", "Hello", theDate2.ToString(), "4", "Goodbye" });
+        }
+
+        [Fact]
+        public void Should_RenderGridForGivenDataSourceWithCustomColumns()
+        {
+            var theDate1 = new DateTime(2023, 06, 03);
+            var theDate2 = new DateTime(2022, 01, 06);
+
+            List<TestModel> testModelList = new()
+            {
+                new TestModel { DateVal = theDate1, NumberVal = 2, TextVal = "Hello" },
+                new TestModel { DateVal = theDate2, NumberVal = 4, TextVal = "Goodbye" }
+            };
+
+            List<string> theColumnList = new() { "DateVal", "TextVal" }; // exclude the NumberVal column
+
+            var ctx = new TestContext();
+            var cut = ctx.RenderComponent<SimpleDataGrid<TestModel>>(parameters => parameters
+                .Add(p => p.DataSource, testModelList)
+                .Add(p => p.ColumnList, theColumnList));
+
+            var table = cut.Find("table");
+            table.Should().NotBeNull();
+
+            var rows = cut.FindAll("tr");
+            rows.Should().NotBeNull();
+            rows.Should().HaveCount(3);
+
+            var headers = cut.FindAll("th");
+            headers.Should().NotBeNull();
+            headers.Should().HaveCount(2);
+            headers.Select(h => h.TextContent).Should().BeEquivalentTo(new[] { "DateVal", "TextVal" });
+
+            var dataCells = cut.FindAll("td");
+            dataCells.Should().NotBeNull();
+            dataCells.Should().HaveCount(4);
+            dataCells.Select(d => d.TextContent).Should().BeEquivalentTo(new[] { theDate1.ToString(), "Hello", theDate2.ToString(), "Goodbye" });
+        }
+
+        [Fact]
+        public void Should_RaiseRowClickEventCallback()
+        {
+            var theDate1 = new DateTime(2023, 06, 03);
+            var theDate2 = new DateTime(2022, 01, 06);
+
+            Dictionary<string, string>? rowData = null;
+
+            List<TestModel> testModelList = new()
+            {
+                new TestModel { DateVal = theDate1, NumberVal = 2, TextVal = "Hello" },
+                new TestModel { DateVal = theDate2, NumberVal = 4, TextVal = "Goodbye" }
+            };
+
+            var ctx = new TestContext();
+            var cut = ctx.RenderComponent<SimpleDataGrid<TestModel>>(parameters => parameters
+                .Add(p => p.DataSource, testModelList)
+                .Add(p => p.OnRowClick, (rowClickData) => rowData = rowClickData));
+
+            var table = cut.Find("table");
+            table.Should().NotBeNull();
+
+            var rows = cut.FindAll("tr");
+            rows[2].Click(); // last row (inc header row)
+
+            rowData.Should().NotBeNull();
+            rowData!["DateVal"].Should().Be(theDate2.ToString());
+            rowData!["NumberVal"].Should().Be("4");
+            rowData!["TextVal"].Should().Be("Goodbye");
+        }
+    }
+}
