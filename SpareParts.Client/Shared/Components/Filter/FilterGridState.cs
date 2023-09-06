e6315c0284df687c40b1512484cb41cab598b515
sjmarsh <sjmarsh31@gmail.com>
2023-09-06 14:52:15 +1000

    Add page number to FilterGridState

--- SpareParts.Client/Shared/Components/Filter/FilterGridState.cs
+++ SpareParts.Client/Shared/Components/Filter/FilterGridState.cs
@@ -8,9 +8,11 @@ namespace SpareParts.Client.Shared.Components.Filter
         {
             FilterFields = ImmutableList<FilterField>.Empty;
             FilterLines = ImmutableList<FilterLine>.Empty;
+            CurrentResultPage = 1;
         }
 
         public ImmutableList<FilterField> FilterFields { get; set; }
         public ImmutableList<FilterLine> FilterLines { get; set; }
+        public int CurrentResultPage { get; set; }
     }
 }
