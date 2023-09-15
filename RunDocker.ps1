a5fe310e961889bebfa079f7bf287ebbada3685e
sjmarsh <sjmarsh31@gmail.com>
2023-09-15 16:08:16 +1000

    Add work-around for Docker Sql connection issue.

--- RunDocker.ps1
+++ RunDocker.ps1
@@ -1,16 +1,23 @@
 # This is a convenience script for setting up docker for SQL Server and the app in local dev.
-$dockerDesktopProcess = Get-Process 'Docker Desktop' -ErrorAction SilentlyContinue
-if($dockerDesktopProcess -eq $null) 
-{
-	Write-Error "Docker Desktop must be installed and running before continuing."
-	Exit
-}
-
+$versionMessage = "PowerShell 7 or higher is required to run this script."
 try {
     Start-Process pwsh -ArgumentList " -command exit"
 } catch {
-    Write-Error "PowerShell 7 or higher is required to run this script."
+    Write-Error "pwsh not available"
+    Write-Error $versionMessage
     Exit
+}
+
+if ($host.Version.Major -lt 7) {
+    Write-Error $versionMessage
+    Exit
+}
+
+$dockerDesktopProcess = Get-Process 'Docker Desktop' -ErrorAction SilentlyContinue
+if ($dockerDesktopProcess -eq $null) 
+{
+	Write-Error "Docker Desktop must be installed and running before continuing."
+	Exit
 }
 
 if ($IsLinux) {
@@ -27,7 +34,19 @@ if((Test-Path -Path $envFile -PathType leaf) -eq $False){
 }
 
 Write-Output "Process environment variables"
+$sqlServerContainer = "sql-server-docker"
+$sparePartsContainer = "spare-parts-image"
+
 $server = hostname
+# This is a work-around for Sql database connection issue that has occurred with recent version of docker and/or sql image
+# TODO: Either fix issue or use a better way of getting the FQDN for the sql server.
+if($IsWindows) {
+    $server = "$server.mshome.net"
+}
+if($IsLinux) {
+    $server = "$server.lan"
+}
+
 $envFileContent = Get-Content -Path $envFile
 $originalEnvFileContent = Get-Content -Path $envFile
 $envFileContent -replace '{SERVER}', $server | Set-Content $envFile
@@ -49,9 +68,14 @@ Set-Content -Path $envFile -Value $envFileContent
 $dockerSqlSaPassword = $devVariables['DOCKER_SQL_SA_PASSWORD']
 $dockerSqlPort = $devVariables['DOCKER_SQL_PORT']
 $devCertPassword = $devVariables['ASPNETCORE_Kestrel__Certificates__Default__Password']
-		
-Write-Output $dockerSqlConnectionString
-	
+
+Write-Output $envFileContent
+
+# Setup docker network
+$sparePartsNetwork = "spare-parts-network"
+Write-Output "Setup Docker Network: $sparePartsNetwork"
+docker network create -d bridge $sparePartsNetwork
+
 # Run Sql Server container
 # Note: array count = 2 because first item contains headers
 $isSqlDockerRunning = @(docker ps -f name=sql).Count -eq 2  
@@ -63,12 +87,12 @@ else {
 	docker pull mcr.microsoft.com/mssql/server
 
 	Write-Output "Run sql server image"
-    Start-Process pwsh -ArgumentList "-noexit -command docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$dockerSqlSaPassword' -p $dockerSqlPort`:1433 --name sql-server-docker -d mcr.microsoft.com/mssql/server"
+    Start-Process pwsh -ArgumentList "-noexit -command docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=$dockerSqlSaPassword' -p $dockerSqlPort`:1433 --name $sqlServerContainer --network $sparePartsNetwork -d mcr.microsoft.com/mssql/server"
 	Start-Sleep -Seconds 10
 }
 	
 Write-Output "Build spare parts image"
-docker build -t spare-parts-image -f SpareParts.API/Dockerfile .
+docker build -t $sparePartsContainer -f SpareParts.API/Dockerfile .
 		
 # TODO this won't work on Linux. Need to implement solution for it.
 if($IsWindows) {
@@ -80,12 +104,12 @@ if($IsWindows) {
 Write-Output "Run spare parts image"
 if($IsWindows) {
     Write-Output "Windows Host"
-    Start-Process pwsh -ArgumentList "-noexit -command docker run --name spare-parts --rm -it -p 8000:80 -p 8001:443 --env-file $envFile -v $env:USERPROFILE/.aspnet/https:/https/ spare-parts-image"
+    Start-Process pwsh -ArgumentList "-noexit -command docker run --name spare-parts --rm -it -p 8000:80 -p 8001:443 --network $sparePartsNetwork --env-file $envFile -v $env:USERPROFILE/.aspnet/https:/https/ $sparePartsContainer"
 }
 if($IsLinux) {
     Write-Output "Linux Host"
-    #Start-Process pwsh -ArgumentList "-noexit -command docker run --name spare-parts --rm -it -p 8000:80 -p 8001:443 --env-file $envFile -v $env:USERPROFILE/usr/lib/ssl/certs:/https/ spare-parts-image"
-    Start-Process pwsh -ArgumentList "-noexit -command docker run --name spare-parts --rm -it -p 8000:80 -p 8001:443 --env-file $envFile spare-parts-image"
+    #Start-Process pwsh -ArgumentList "-noexit -command docker run --name spare-parts --rm -it -p 8000:80 -p 8001:443 --env-file $envFile -v $env:USERPROFILE/usr/lib/ssl/certs:/https/ $sparePartsContainer"
+    Start-Process pwsh -ArgumentList "-noexit -command docker run --name spare-parts --rm -it -p 8000:80 -p 8001:443 --network $sparePartsNetwork --env-file $envFile $sparePartsContainer"
 }
 
 #Ping app to see if it is up yet
